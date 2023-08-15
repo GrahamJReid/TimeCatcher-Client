5cbd6fc61501c15a3257b36fc6cd2ae47a92baf9
GrahamJReid <grahamreidtechnology@gmail.com>
2023-08-15 10:48:52 -0500

    navbar switched to Off canvas component

---
+++ API/userData.js
@@ -0,0 +1,14 @@
+import { clientCredentials } from '../utils/client';
+
+const getSingleUser = (id) => new Promise((resolve, reject) => {
+  fetch(`${clientCredentials.databaseURL}/users/${id}`, {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  })
+    .then((response) => response.json())
+    .then((data) => resolve((data)))
+    .catch(reject);
+});
+export default getSingleUser;
