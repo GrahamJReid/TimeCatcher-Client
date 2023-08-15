9c4c75b5570a23493204729fa763f726b2dc1748
GrahamJReid <grahamreidtechnology@gmail.com>
2023-08-15 11:43:25 -0500

    home page parallax basic setup complete

--- pages/index.js
+++ pages/index.js
@@ -1,41 +1,36 @@
-/* eslint-disable jsx-a11y/alt-text */
+/* eslint-disable @next/next/no-css-tags */
 /* eslint-disable @next/next/no-img-element */
-import { Button } from 'react-bootstrap';
-import { useEffect, useState } from 'react';
-import { signOut } from '../utils/auth';
-import { useAuth } from '../utils/context/authContext';
-import getSingleUser from '../utils/data/userData';
-
 function Home() {
-  const { user } = useAuth();
-  const [singleUser, setSingleUser] = useState({});
-  console.warn('this is me singleUser', singleUser);
-  const absoluteImageUrl = `${singleUser.image}`;
-
-  useEffect(() => {
-    getSingleUser(user.id).then(setSingleUser);
-  // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
-
   return (
-    <div
-      className="text-center d-flex flex-column justify-content-center align-content-center"
-      style={{
-        height: '90vh',
-        padding: '30px',
-        maxWidth: '400px',
-        margin: '0 auto',
-      }}
-    >
-      <h1>Hello {user.username}! </h1>
+    <>
+      <div className="parallax_wrapper">
+        <div className="parallax_group intro_screen" id="intro">
+          Welcome user!
+        </div>
 
-      <img src={absoluteImageUrl} />
-      <p>Your Bio: {user.bio}</p>
-      <p>Click the button below to logout!</p>
-      <Button variant="danger" type="button" size="lg" className="copy-btn" onClick={signOut}>
-        Sign Out
-      </Button>
-    </div>
+        <div className="parallax_group" id="group-1">
+          <div className="parallax_layer base_layer">
+            base layer page
+          </div>
+          <div className="parallax_layer mid_layer">
+            Gallery
+          </div>
+        </div>
+
+        <div className="parallax_group" id="group-2">
+          <div className="parallax_layer mid_layer">
+            <div>mid Layer</div>
+          </div>
+          <div className="parallax_layer top_layer">
+            Image Form
+          </div>
+        </div>
+
+        <div className="parallax_group outro_screen" id="outro">
+          outro screen
+        </div>
+      </div>
+    </>
   );
 }
 
