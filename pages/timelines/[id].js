4c130c0cc498520c23bc89ee80ff87e7e8ddd5c2
GrahamJReid <grahamreidtechnology@gmail.com>
2023-08-20 09:27:43 -0500

    when viewing another users profile users are able to add entire timelines to their collection

--- pages/timelines/[id].js
+++ pages/timelines/[id].js
@@ -10,9 +10,9 @@ import { Button, Dropdown } from 'react-bootstrap';
 import {
   createTimelineEvent, deleteTimelineEvent, getSingleTimelineEvents, getTimelineEventsByEventId,
 } from '../../API/timelineEvent';
-import { getUserEvents } from '../../API/eventData';
+import { createEvent, getUserEvents } from '../../API/eventData';
 import { useAuth } from '../../utils/context/authContext';
-import { getSingleTimeline } from '../../API/timelineData';
+import { createTimeline, getSingleTimeline } from '../../API/timelineData';
 import EventFormModal from '../../components/events/EventFormModal';
 
 function Timeline() {
@@ -75,6 +75,64 @@ function Timeline() {
       // You can perform further operations on this filtered data
     });
   };
+  const handleAddToCollection = async () => {
+    try {
+      // Create a new timeline with the current user as the owner
+      const newTimeline = {
+        title: timeline.title,
+        imageUrl: timeline.image_url,
+        public: timeline.public,
+        gallery: timeline.gallery,
+        dateAdded: timeline.date_added,
+        userId: user.id, // Set the owner to the current user
+      };
+
+      // Create the new timeline
+      const createdTimeline = await createTimeline(newTimeline);
+
+      // Use Promise.all to duplicate events asynchronously
+      const eventPromises = sortedEventArray.map(async (event) => {
+        const newEvent = {
+          title: event.title,
+          description: event.description,
+          date: event.date,
+          color: event.color,
+          imageUrl: event.image_url,
+          userId: user.id, // Set the owner to the current user
+        };
+
+        // Create the new event for the new timeline and return the promise
+        return createEvent(newEvent);
+      });
+
+      // Wait for all event creation promises to resolve
+      const createdEvents = await Promise.all(eventPromises);
+
+      // Use Promise.all to create timeline events linking events to the timeline
+      const timelineEventPromises = createdEvents.map(async (createdEvent) => {
+        const timelineEvent = {
+          timelineId: createdTimeline.id,
+          eventId: createdEvent.id, // Use the ID of the newly created event
+        };
+
+        // Create the timeline event and return the promise
+        return createTimelineEvent(timelineEvent);
+      });
+
+      // Wait for all timeline event creation promises to resolve
+      await Promise.all(timelineEventPromises);
+
+      // Optionally, you can redirect the user to their collection of timelines
+      router.push('/timelines/MyTimelines');
+
+      // Log the result
+      console.warn('Timeline and events successfully added to the collection:', createdTimeline);
+    } catch (error) {
+      // Handle any errors that occur during the process
+      console.error('Error adding timeline to the collection:', error);
+    }
+  };
+
   console.warn('are these equal', user.id, timeline.user_id);
   return (
     <div>
@@ -94,7 +152,14 @@ function Timeline() {
           </Dropdown>
           <EventFormModal />
         </>
-      ) : ''}
+      ) : (
+        <Button
+          onClick={handleAddToCollection}
+          className="event-card-button"
+        >
+          Add
+        </Button>
+      )}
       <VerticalTimeline>
         <h1>{timeline.title}</h1>
         <div>
